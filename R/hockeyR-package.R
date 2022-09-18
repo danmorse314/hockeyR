b32490d5aab5ef5f7add8f3f380c94a4d561e154
Dan Morse <danmorse8642@gmail.com>
2022-09-18 09:01:20 -0700

    fixedpackage documentation that threw html5 error

--- R/hockeyR-package.R
+++ R/hockeyR-package.R
@@ -1,7 +1,6 @@
 #' @keywords internal
+#' @description Various functions to scrape hockey play-by-play data from the
+#' <https://www.nhl.com/>. It also contains functions to scrape data from <https://www.hockey-reference.com/>,
+#' including standings, player stats, and jersey number history.
 "_PACKAGE"
-
-## usethis namespace: start
-#' @importFrom stats predict
-## usethis namespace: end
 NULL
